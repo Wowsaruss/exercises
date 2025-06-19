6e35707a7c0ccfe1e4137e4467dec42981c61dde
Wowsaruss <russell.hayes.dev@gmail.com>
2025-06-19 11:39:12 -0700

    completed

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,16 +1,18 @@
 export function hey(message: string): string {
-  const messageArray = message.split("");
-  const messageLength = message.length;
-  const isUpperCase = message === message.toUpperCase()
-  const allWhitespace = message.trim() === "";
+  const trimmed = message.trim();
+  const messageArray = trimmed.split("");
+  const messageLength = trimmed.length;
+  const isUpperCase = trimmed === trimmed.toUpperCase()
+  const isLowerCase = trimmed === trimmed.toLowerCase()
+  const allWhitespace = trimmed === "";
 
   if (allWhitespace) {
     return "Fine. Be that way!";
-  } else if (messageArray[messageLength - 1] === "?" && isUpperCase) {
+  } else if ((messageArray[messageLength - 1] === "?") && (isUpperCase && !isLowerCase)) {
     return "Calm down, I know what I'm doing!";
   } else if (messageArray[messageLength - 1] === "?") {
     return "Sure.";
-  } else if (isUpperCase) {
+  } else if (isUpperCase && !isLowerCase) {
     return "Whoa, chill out!";
   }
 
