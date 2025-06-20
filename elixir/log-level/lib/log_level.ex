cfe4a4191d3c774aeb85664dfe970b71929a19b0
Wowsaruss <russell.hayes.dev@gmail.com>
2025-06-20 16:54:17 -0700

    complete

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,9 +1,49 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    {level, legacy?}
+    |> case do
+      {0, false} ->
+        :trace
+      {1, _} ->
+        :debug
+      {2, _} ->
+        :info
+      {3, _} ->
+        :warning
+      {4, _} ->
+        :error
+      {5, false} ->
+        :fatal
+      _ ->
+        :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    {level, legacy?}
+    |> case do
+      {0, false} ->
+        false
+      {0, true} ->
+        :dev1
+      {1, _} ->
+        false
+      {2, _} ->
+        false
+      {3, _} ->
+        false
+      {4, _} ->
+        :ops
+      {5, true} ->
+        :dev1
+      {5, false} ->
+        :ops
+      {6, true} ->
+        :dev1
+      {6, false} ->
+        :dev2
+      _ ->
+        :unknown
+    end
   end
 end
